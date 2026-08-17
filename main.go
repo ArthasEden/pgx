d6cc17594b1421ad247291986f671eea5df88c32
Arthas <arthas@example.com>
2026-08-17 10:59:19 +0600

    add pgxpool

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
 
 func main() {
-	fmt.Println("Hello pgx!")
+	ctx := context.Background()
+	connStr := "postgres://postgres:pass@localhost:5432/postgres"
+
+	pool, err := pgxpool.New(ctx, connStr)
+	if err != nil {
+		fmt.Println("Can't connect:", err)
+		return
+	}
+	defer pool.Close()
+
+	if err := pool.Ping(ctx); err != nil {
+		fmt.Println("Can't ping connection:", err)
+		return
+	}
+
+	fmt.Println("Successfully connected to DB!")
 }
